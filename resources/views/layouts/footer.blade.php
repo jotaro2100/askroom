12eb5c18a281cb06374b16a2109b238110dd5811
jotaro2100 <pro.jotaro2100@gmail.com>
2023-09-16 20:18:16 +0900

    フッターのコンポーネントを作成

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-transparent py-4 w-screen absolute bottom-0 border-t border-solid border-gray-500">
+    <div class="text-gray-500 text-center">
+        <span class="">Copyright © Askroom 2023 All rights reserved</span>
+    </div>
+</footer>
