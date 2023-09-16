5da36c9fe9c6aa4c5f12db5a9e701c919fc0d9fa
jotaro2100 <pro.jotaro2100@gmail.com>
2023-09-16 20:08:42 +0900

    Queryコントローラーを作成

---
+++ app/Http/Controllers/QueryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Query;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class QueryController extends Controller
+{
+}
