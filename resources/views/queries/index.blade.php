a04f66a5a8af7b5395c53f8626499ac940f6195b
jotaro2100 <pro.jotaro2100@gmail.com>
2023-09-19 03:27:22 +0900

    ビューの細かな調整

--- resources/views/queries/index.blade.php
+++ resources/views/queries/index.blade.php
@@ -2,23 +2,23 @@
     <div class="py-12">
         <div class="px-2 max-w-3xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div>
-                <h1 class="flex justify-between mb-8 text-3xl">
+                <h1 class="flex justify-between mb-8 text-3xl items-center">
                     <span class="text-gray-900 dark:text-white border-b-[1px]">{{ $title }}</span>
-                    <a href="{{ route('queries.create') }}" type="button" class="block px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">新しい質問</a>
+                    <a href="{{ route('queries.create') }}" type="button" class="block px-3 py-2 text-sm font-bold text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">新しい質問</a>
                 </h1>
             </div>
             @forelse ($queries as $query)
                 <div>
-                    <a href="{{ route('queries.show', $query) }}" class="block h-[200px] mb-4 p-6 bg-white border-[2px] border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-900 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <a href="{{ route('queries.show', $query) }}" class="block !h-[224px] mb-4 px-6 py-4 bg-white border-[2px] border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-slate-900 dark:border-gray-700 dark:hover:bg-gray-700">
                         <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
                             {{ $query->title }}
                         </h5>
                         <p class="mb-2 font-bold text-gray-700 dark:text-gray-300">
                             投稿者： {{ $query->user->name }}
                         </p>
-                        <p class="font-normal text-gray-700 dark:text-gray-400">
-                            {{ $query->content }}
-                        </p>
+                        <div class="max-h-[120px] font-normal text-gray-700 dark:text-gray-400 overflow-hidden">
+                            {!! nl2br(e($query->content)) !!}
+                        </div>
                     </a>
                 </div>
             @empty
