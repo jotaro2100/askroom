2d70d2afc0956f6a4479778367c1850b08addbd8
jotaro2100 <pro.jotaro2100@gmail.com>
2023-09-19 14:16:27 +0900

    検索バーのレイアウト変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- FontAwesome -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
--- resources/views/queries/index.blade.php
+++ resources/views/queries/index.blade.php
@@ -13,16 +13,14 @@
                         <div class="flex items-center">
                             <div class="bg-gray-900 flex-grow">
                                 <div class="container mx-auto">
-                                    <div class="relative text-gray-600">
-                                        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><style>svg{fill:#bbb}</style><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
-                                        </div>
-                                        <input type="search" placeholder="{{$title}}を検索" name="search" value="@if (isset($search)) {{ $search }} @endif" class="bg-gray-800 text-white border-2 border-gray-700 focus:outline-none focus:bg-gray-700 rounded-lg py-2 pl-10 pr-4 w-full leading-5 transition-colors duration-300">
+                                    <div class="flex items-center text-gray-600">
+                                        <i class="fa-solid fa-magnifying-glass fa-lg mr-3" style="color: #bbb;"></i>
+                                        <input type="search" placeholder="{{$title}}を検索" name="search" value="@if (isset($search)) {{ $search }} @endif" class="bg-gray-800 text-white border-2 border-gray-700 focus:outline-none focus:bg-gray-700 rounded-lg py-2 pl-4 pr-4 w-full leading-5 transition-colors duration-300">
                                     </div>
                                 </div>
                             </div>
                             <div class="text-white">
-                                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-md text-sm px-4 py-2 mx-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">検索</button>
+                                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-md text-sm px-4 py-2 mx-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">検索</button>
                                 <button class="align-middle">
                                     <a href="{{ route($controller . '.' . $method) }}" class="text-red-500">クリア</a>
                                 </button>
