494a1a9a4df3ac7653e6d28f20869609062453ed
jotaro2100 <pro.jotaro2100@gmail.com>
2023-10-14 18:44:50 +0900

    フラッシュメッセージのデザインと挙動を変更

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,10 +1,17 @@
 @if (session('flash_message'))
-    <div id="flash" class="!bg-green-500 absolute w-full text-center text-white py-1 text-sm font-bold z-10">
-        {{ session('flash_message') }}
+    <div id="flash" class="mt-4 !bg-slate-800 border rounded-lg border-green-600 shadow-lg fixed text-center text-green-600 px-3 py-2 text-sm font-bold !z-50">
+        <span>
+            <i class="fa-solid fa-circle-check mr-1"></i>
+            {{ session('flash_message') }}
+        </span>
     </div>
 @endif
+
 @if ($errors->any())
-    <div id="flash" class="!bg-red-500 absolute w-full text-center text-white py-1 text-sm font-bold z-10">
-        投稿に失敗しました
+    <div id="flash" class="mt-4 !bg-slate-800 border rounded-lg border-red-400 shadow-lg fixed text-center text-red-400 px-3 py-2 text-sm font-bold !z-50">
+        <span>
+            <i class="fa-solid fa-triangle-exclamation mr-1"></i>
+            投稿に失敗しました
+        </span>
     </div>
 @endif
